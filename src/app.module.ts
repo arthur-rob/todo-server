f989a33d1ea06b38094eb8fa9cf81364f7526c2a
Arthur <arthur.rob.dev@gmail.com>
2025-05-15 18:40:11 +0200

    fix(task): add Task to typeorm entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/entities/user.entity';
+import { Task } from './task/entities/task.entity';
 import { UsersModule } from './users/users.module';
 import { LoggerMiddleware } from './common/logger.middleware';
 import { AuthModule } from './auth/auth.module';
@@ -17,7 +18,7 @@ import { TaskModule } from './task/task.module';
             username: process.env.DB_USER_PASSWORD || 'root',
             password: process.env.DB_USER_PASSWORD || '',
             database: process.env.DB_NAME || 'todo',
-            entities: [User],
+            entities: [User, Task],
             synchronize: true,
         }),
         UsersModule,

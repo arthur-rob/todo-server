2c770501fac8cabe42d4e78f7664f1cb51792976
Arthur <arthur.rob.dev@gmail.com>
2025-05-15 12:16:36 +0200

    feature(auth): add auth module and implement login route

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
 import { LoggerMiddleware } from './common/logger.middleware';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
     imports: [
@@ -19,6 +20,7 @@ import { LoggerMiddleware } from './common/logger.middleware';
             synchronize: true,
         }),
         UsersModule,
+        AuthModule,
     ],
     controllers: [AppController],
     providers: [AppService],

c3bf7927d00eb001c7ec2f7fa31dfd2dd7a5c3af
Arthur <arthur.rob.dev@gmail.com>
2025-05-15 12:31:25 +0200

    feature(auth): add profile route, retrieve user with token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,16 @@
-import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    Request,
+    Post,
+    HttpCode,
+    HttpStatus,
+    UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
+import { User } from 'src/users/entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +20,11 @@ export class AuthController {
     @Post('login')
     login(@Body() signInDto: Record<string, any>) {
         return this.authService.login(signInDto.email, signInDto.password);
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('profile')
+    getProfile(@Request() req: Request & { user: User }): User {
+        return req.user;
     }
 }
